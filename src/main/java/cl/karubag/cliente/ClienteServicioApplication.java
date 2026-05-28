5d39c2ecd82b67b47873494b455f121971b7fa96
klaw3134 <cgnavarrete3134@gmail.com>
2026-05-28 13:36:30 -0400

    init: cliente-servicio

---
+++ src/main/java/cl/karubag/cliente/ClienteServicioApplication.java
@@ -0,0 +1,13 @@
+package cl.karubag.cliente;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClienteServicioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClienteServicioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cl/karubag/cliente/ClienteServicioApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.karubag.cliente;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClienteServicioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
